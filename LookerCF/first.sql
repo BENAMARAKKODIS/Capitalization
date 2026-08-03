7b1ad2462ae1e0902319c628d76c8701654be482
akkodis-menaps <alexis.moulin-extern@ampere.cars>
2026-08-03 10:44:30 +0200

    modifs suite à réunion

--- LookerCF/first.sql
+++ LookerCF/first.sql
@@ -1,3 +1,5 @@
+-- 1. Overall VIES tickets view per domain for MyF3+
+
 -- DOR Opinion
 case(
   when(${amq_s_capitalization_preprod.vies_dor_opinion} = "VIES not to capitalise", "3 - VIES not to capitalise"),
@@ -9,14 +11,14 @@ case(
 -- Sorted Domains
 case(
   when(${amq_s_capitalization_preprod.vies_domain} = "Multimedia", "1. Multimedia"),
-  when(${amq_s_capitalization_preprod.vies_domain} = "Connectivity On-Board", "2. Connectivity On-Board"),
-  when(${amq_s_capitalization_preprod.vies_domain} = "Connectivity Off-Board", "3. Connectivity Off-Board"),
-  when(${amq_s_capitalization_preprod.vies_domain} = "Meter", "4. Meter"),
+  when(${amq_s_capitalization_preprod.vies_domain} = "Meter", "2. Meter"),
+  when(${amq_s_capitalization_preprod.vies_domain} = "Connectivity On-Board", "3. Connectivity On-Board"),
+  when(${amq_s_capitalization_preprod.vies_domain} = "Connectivity Off-Board", "4. Connectivity Off-Board"),
   when(${amq_s_capitalization_preprod.vies_domain} = "ADAS", "5. ADAS"),
   when(${amq_s_capitalization_preprod.vies_domain} = "e-Body", "6. e-Body"),
-  when(${amq_s_capitalization_preprod.vies_domain} = "e-Chassis", "7. e-Chassis"),
-  when(${amq_s_capitalization_preprod.vies_domain} = "Wire Harness", "8. Wire Harness"),
-  when(${amq_s_capitalization_preprod.vies_domain} = "Battery", "9. Battery"),
-  when(${amq_s_capitalization_preprod.vies_domain} = "Electrotechnical", "A. Electrotechnical"),
+  when(${amq_s_capitalization_preprod.vies_domain} = "Wire Harness", "7. Wire Harness"),
+  when(${amq_s_capitalization_preprod.vies_domain} = "Battery", "8. Battery"),
+  when(${amq_s_capitalization_preprod.vies_domain} = "Electrotechnical", "9. Electrotechnical"),
+  when(${amq_s_capitalization_preprod.vies_domain} = "e-Chassis", "A. e-Chassis"),
   "Other"
 )
